de5f014f0ca765e20b7bddffa227fa3791718909
Eonist <30n1st@gmail.com>
2019-04-28 08:15:47 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+[![Carthage compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)](https://github.com/Carthage/Carthage)
+[![Swift-lint Sindre](https://img.shields.io/badge/swift--lint-sindre-brightgreen.svg)](https://github.com/sindresorhus/swiftlint-sindre)
+
+
 # ReusableCell
 ReusableCell
+
+### Example:
+
+```swift
+extension CustomCell: ReusableView {} // Make your custom cells
+let cell: CustomCell = collectionView.dequeueReusableCell(for: indexPath) // Instantiate your custom cells
+```
+
+### Install:
+- Carthage: `github "eonist/ReusableCell" "master"`
+- Manual: Open `ReusableCellExample.xcodeproj`
+- CocoaPod (Coming soon)  
--- ReusableCellExample/src/UITable+Reusable.swift
+++ ReusableCellExample/src/UITable+Reusable.swift
@@ -1,10 +1,10 @@
 import UIKit
 
 extension UITableView {
-    func register<T: UITableViewCell>(_: T.Type) where T: ReusableCell {
+    public func register<T: UITableViewCell>(_: T.Type) where T: ReusableCell {
         register(T.self, forCellReuseIdentifier: T.defaultReuseIdentifier)
     }
-    func register<T: UITableViewCell>(_: T.Type) where T: ReusableCell, T: NibLoadableView {
+    public func register<T: UITableViewCell>(_: T.Type) where T: ReusableCell, T: NibLoadableView {
         let bundle = Bundle(for: T.self)
         let nib = UINib(nibName: T.nibName, bundle: bundle)
         register(nib, forCellReuseIdentifier: T.defaultReuseIdentifier)
@@ -13,11 +13,11 @@ extension UITableView {
     /// ## Examples:
     /// let cell: CustomCell = collectionView.dequeueReusableCell(for: indexPath)
     ///
-    func dequeueReusableCell<T: UITableViewCell>() -> T where T: ReusableCell, T: NibLoadableView {
+    public func dequeueReusableCell<T: UITableViewCell>() -> T where T: ReusableCell, T: NibLoadableView {
         register(T.self)
         return dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier) as! T
     }
-    func dequeueReusableCell<T: UITableViewCell>() -> T where T: ReusableCell {
+    public func dequeueReusableCell<T: UITableViewCell>() -> T where T: ReusableCell {
         register(T.self)
         return dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier) as! T
     }
