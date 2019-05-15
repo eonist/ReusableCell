3683293618d16a9cc5a7e66aa41eb22a7efd2a00
andre.jorgensen <andre.jorgensen@tidal.com>
2019-05-15 12:31:44 +0200

    Add support for registering multiple cells types

--- ReusableCellExample/src/UITable+Reusable.swift
+++ ReusableCellExample/src/UITable+Reusable.swift
@@ -26,11 +26,31 @@ extension UITableView {
         return dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier) as! T
     }
 }
+///
+/// Adds indexPath support
+///
 extension UITableView{
     public func dequeueReusableCell<T: UITableViewCell>(indexPath: IndexPath) -> T where T: ReusableCellKind {
         return dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier, for: indexPath) as! T
     }
     public func dequeueReusableCell<T: UITableViewCell>(indexPath: IndexPath) -> T where T: ReusableCellKind, T: NibLoadableView {
         return dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier, for: indexPath) as! T
+    }
+}
+/**
+ * Bulk support for registering class type
+ */
+extension UITableView {
+    ///
+    /// ## Example:
+    /// tableView.register([CustomCell.self, OtherCustomCell.self, ThirdCustomCell.self])
+    ///
+    public func register<T: UITableViewCell>(_: [T.Type]) where T: ReusableCellKind {
+        register(T.self, forCellReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    public func register<T: UITableViewCell>(_: [T.Type]) where T: ReusableCellKind, T: NibLoadableView {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        register(nib, forCellReuseIdentifier: T.defaultReuseIdentifier)
     }
 }
